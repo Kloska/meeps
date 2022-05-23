26e1449be6a44246b0cb8a4371a5af2868a51d0a
Kloska <tim.lundstrom@elev.ga.ntig.se>
2022-05-23 10:35:28 +0200

    routes försök

--- routes/meeps.js
+++ routes/meeps.js
@@ -2,13 +2,6 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
-router.get('/',
-  async (req, res, next) => {
-    const sql = 'SELECT * FROM meeps';
-    result = await query(sql);
-    res.send(result);
-});
-
 /* router.get('/', async (req, res, next) => {
     await pool
         .promise()
